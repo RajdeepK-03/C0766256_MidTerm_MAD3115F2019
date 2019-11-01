72ccba7d47196f020f66c9ec4f260e71fab63172
Rajdeep <chahal.rajdeep03@gmail.com>
2019-11-01 17:02:09 -0400

    design modified

--- C0766256_MidTerm_MAD3115F2019/BillDetailsViewController.swift
+++ C0766256_MidTerm_MAD3115F2019/BillDetailsViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class BillDetailsViewController: UIViewController {
 
     
-    var billInformation : Customer? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
 
