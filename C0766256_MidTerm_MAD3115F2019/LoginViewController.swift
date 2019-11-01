b23d3f6f0e1d3f92d981660677ef1d125cd6de2c
Rajdeep Kaur <chahal.rajdeep03@gmail.com>
2019-10-31 21:26:17 -0400

    new

--- C0766256_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0766256_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+
+    @IBOutlet weak var Email: UITextField!
     
-    @IBOutlet var Email: UIView!
     
-    
-    @IBOutlet var Password: UIView!
+    @IBOutlet weak var Password: UITextField!
     
     
     @IBOutlet weak var RememberMeSwitch: UISwitch!
@@ -24,19 +24,41 @@ class LoginViewController: UIViewController {
     }
     @IBAction func LoginButton(_ sender: UIBarButtonItem) {
         
+    
+        let userEmail = Email.text!
+        let password = Password.text!
+        if password.count < 6
+        {
         
-        
-        
-        
-        
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-        let CustomerVC = sb.instantiateViewController(withIdentifier: "CustomerVC") as! CustomerListTableViewController
+        if userEmail == "rajdeep@gmail.com" && password == "123"
+        {
+                print("User Successfully login")
+        performSegue(withIdentifier: "User Sucessfully login", sender: nil)
+                }
+            else{
+                    print("Incorrect Password")
+                }
+            }
+        else{
+                print("Incorrect Email")
+            }
+        }
+      else{
+            let alert = UIAlertController(title: "Email Error", message: "Your Email is less than 10 charachter. Please enter a valid email", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Ignore", style: .destructive, handler: nil))
+            self.present(alert, animated: true)
+        }
+    
+
+        //let sb = UIStoryboard(name: "Main", bundle: nil)
+        //let CustomerVC = sb.instantiateViewController(withIdentifier: "CustomerVC") as! CustomerListTableViewController
         
 
-        navigationController?.pushViewController(CustomerVC, animated: true)
+        //navigationController?.pushViewController(CustomerVC, animated: true)
         
     
 
-}
 }
 
