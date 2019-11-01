23cee8ba4de11bc9ec27cf9937d35a893f36a9b9
Rajdeep <chahal.rajdeep03@gmail.com>
2019-11-01 14:46:48 -0400

    additions in table view

--- C0766256_MidTerm_MAD3115F2019/CustomerListTableViewController.swift
+++ C0766256_MidTerm_MAD3115F2019/CustomerListTableViewController.swift
@@ -55,7 +55,18 @@ class CustomerListTableViewController: UIViewController,UITableViewDelegate,UITa
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return temp.returnCount()
     }
-    /*
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let x = temp.returnCustObject(customerID: Int(indexPath.row+1))
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = x?.fullName
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Customer's List"
+           /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
